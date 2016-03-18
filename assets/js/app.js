c0e3d632db8fcef6132f5a51510e9f6ea80d8bca
Ton That Quoc Cuong <ttquoccuong@gmail.com>
2016-03-18 16:41:20 +0700

    changed style on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,30 +6,17 @@ $('ul li.dropdown').hover(function() {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 })
-
 $('.owl-carousel').owlCarousel({
 	loop:true,
 	margin: 20,
+	nav: true,
 	navText: [ '<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>' ],
 	responsiveClass:true,
 	responsive: {
-		0: {
-			items: 1,
-			nav: false
-		},
-		480: {
-			items: 2,
-			nav: false
-		},
-		768: {
-			items: 3,
-			nav: false
-		},
-		992: {
-			items: 4,
-			nav: true
-		}
+		0: { items: 1 },
+		480: { items: 2 },
+		768: { items: 3 },
+		992: { items: 4 }
 	}
 });
-
 document.getElementById('copyright').innerHTML = "&copy; " + new Date().getFullYear() + " Quỹ Chống hàng giả";
